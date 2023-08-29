d8fe59cdb6c77929d95772126a6bac1f6fba5362
Matheus Arantes <matheusarantes10@hotmail.com>
2023-08-29 00:24:08 -0300

    Ajustes lint 2

--- src/services/user.js
+++ src/services/user.js
@@ -20,11 +20,27 @@ const createUser = async (user) => {
 };
 
 const findAll = async () => {
-    const users = await User.findAll({ attributes : { exclude: ['password'] } });
+    const users = await User.findAll({ attributes: { exclude: ['password'] } });
     return { status: 200, data: users };
+};
+
+const findById = async (id) => {
+    const user = await User.findOne({
+        where: {
+          id: id,
+          attributes : { exclude: ['password'] }
+        }
+    });
+    
+    if (!user) {
+        return { status: 404, data: { message: 'User does not exist' } };
+    }
+
+    return {}
 }
 
 module.exports = {
     createUser,
     findAll,
+    findById,
 };

9835c43898f2a10d6bd4dd1e69a96d15afb2fbb9
Matheus Arantes <matheusarantes10@hotmail.com>
2023-08-28 22:27:41 -0300

    Validacao do token

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+
+    try {
+      jwt.verify(token, process.env.JWT_SECRET)
+      next();
+    } catch (error) {
+      res.status(401).json({ message: 'Expired or invalid token' });
+    }
+
+  };
+  
+module.exports = {
+   verifyToken,
+};
